Pierre2324/NGK_android_kernel_xiaomi_sm8250

cpu_input_boost: prevent boosting to a freq lower than min
Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -98,11 +98,11 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = input_boost_freq_lp;
+		freq = max(input_boost_freq_lp, remove_input_boost_freq_lp);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = input_boost_freq_hp;
+		freq = max(input_boost_freq_hp, remove_input_boost_freq_perf);
 	else
-		freq = input_boost_freq_pp;
+		freq = max(input_boost_freq_pp, remove_input_boost_freq_pp);
 	return min(freq, policy->max);
 }
 
@@ -111,11 +111,11 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = max_boost_freq_lp;
+		freq = max(max_boost_freq_lp, remove_input_boost_freq_lp);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = max_boost_freq_hp;
+		freq = max(max_boost_freq_hp, remove_input_boost_freq_perf);
 	else
-		freq = max_boost_freq_pp;
+		freq = max(max_boost_freq_pp, remove_input_boost_freq_pp);
 	return min(freq, policy->max);
 }
 

angelokimhui/kernel_xiaomi_apollo

Signed-off-by: Angelo Kim Hui Lim <angelokimhui@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -98,11 +98,11 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = input_boost_freq_little;
+		freq = max(input_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = input_boost_freq_big;
+		freq = max(input_boost_freq_big, cpu_freq_min_big);
 	else
-		freq = input_boost_freq_prime;
+		freq = max(input_boost_freq_prime, cpu_freq_min_prime);
 	return min(freq, policy->max);
 }
 
@@ -111,11 +111,11 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = max_boost_freq_little;
+		freq = max(max_boost_freq_little, cpu_freq_min_little);
 	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = max_boost_freq_big;
+		freq = max(max_boost_freq_big, cpu_freq_min_big);
 	else
-		freq = max_boost_freq_prime;
+		freq = max(max_boost_freq_prime, cpu_freq_min_prime);
 	return min(freq, policy->max);
 }
 